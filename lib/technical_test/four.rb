18371102506a3fb3a6dac911f08006b0fe7d7ab2
Tony KINO <36470087+tonykino@users.noreply.github.com>
2019-02-18 13:15:33 +0100

    Delete four.rb

--- lib/technical_test/four.rb
+++
@@ -1,7 +0,0 @@
-class TechnicalTest::Four
-  def largest_number_from_array(array)
-  	combination_of_orders = array.permutation.to_a
-  	array_of_possibilities = combination_of_orders.map { |order| order.join.to_i }
-		array_of_possibilities.max.to_s
-  end
-end
