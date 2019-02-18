5dcabf152aca25a868862b6d39d1f8623f57e5e9
Tony KINO <tony.kino.3@gmail.com>
2019-02-18 14:56:49 +0100

    Third exercice

---
+++ lib/technical_test/three.rb
@@ -0,0 +1,13 @@
+class TechnicalTest::Three
+  def fibo(number_of_element)
+    return nil if number_of_element < 0
+    return 0 if number_of_element == 0
+    return 1 if number_of_element == 1
+    fibo = [0, 1]
+    (number_of_element-1).times do
+      fibo << fibo[-1] + fibo[-2]
+      p fibo
+    end
+    fibo.last
+  end
+end
